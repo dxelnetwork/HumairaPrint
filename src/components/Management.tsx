b814cd32c66a1762cddf705ab9331c4a4e38b706
DXEL Network <dxel@outlook.com>
2025-06-27 00:02:18 +0600

    Updated Management.tsx

--- src/components/Management.tsx
+++ src/components/Management.tsx
@@ -68,7 +68,7 @@ const Management = () => {
                   <Mail className="h-5 w-5 text-white" />
                 </button>
                 <button className="p-3 bg-green-600 hover:bg-green-700 rounded-lg transition-colors duration-300">
-                  <Phone className="h-5 w-5 text-white" />
+                  <Phone className="h-5 w-5 text-white" href="tel:+4733378901" />
                 </button>
               </div>
             </div>
